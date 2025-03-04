18b48c5f9a6b4f2de79c57f7e31a0345f20de353
Diego Panqueva <diego.panqueva@glinttglobal.com>
2025-03-04 15:20:11 +0100

    Initial commit

---
+++ src/test/java/com/dieva/config/mcsv/ConfigMcsvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dieva.config.mcsv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigMcsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
